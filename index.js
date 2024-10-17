25ce4624561c8932eb38bd358a2a66524cfd12c2
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-18 00:28:02 +0800

    更新

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const execFileSync = require('node:child_process').execFileSync;
+const core = require('@actions/core')
+const github = require('@actions/github')
+const execFileSync = require('node:child_process').execFileSync
 
 async function run() {
-    const token = core.getInput('github-token', {required: true});
-    const octokit = github.getOctokit(token);
+    const token = core.getInput('github-token', { required: true })
+    const octokit = github.getOctokit(token)
 
-    const context = github.context;
-    const username = context.payload.review.user.login;
-    const body = context.payload.pull_request.body ?? "";
+    const context = github.context
+    const username = context.payload.review.user.login
+    const body = context.payload.pull_request.body ?? ""
 
-    const {data: user} = await octokit.rest.users.getByUsername({
+    const { data: user } = await octokit.rest.users.getByUsername({
         username,
-    });
+    })
 
-    var reviewer;
+    var reviewer
     if (user.name) {
         if (user.email) {
-            reviewer = `${user.name} <${user.email}>`;
+            reviewer = `[${user.name}](https://github.com/${username}) <${user.email}>`
         } else {
-            reviewer = `${user.name}`;
+            reviewer = `[${user.name}](https://github.com/${username})`
         }
     } else {
-        reviewer = `${username}`;
+        reviewer = `[${username}](https://github.com/${username})`
     }
 
-    const bodyWithNewline = body.endsWith('\n') ? body : body + '\n';
+    const bodyWithNewline = body.endsWith('\n') ? body : body + '\n'
 
-    const reviewedBy = `Reviewed-by: ${reviewer}`;
+    const reviewedBy = `Reviewed-by: ${reviewer}`
 
-    console.log(reviewedBy);
+    console.log(reviewedBy)
 
     bodyWithTrailer = execFileSync('git', ['interpret-trailers',
         '--no-divider', '--where', 'after', '--trailer', reviewedBy],
-        {input: bodyWithNewline}).toString();
+        { input: bodyWithNewline }).toString()
 
     octokit.rest.pulls.update({
         owner: context.repo.owner,
@@ -44,4 +44,4 @@ async function run() {
 }
 
 run()
-.catch(error => core.setFailed(error.message))
+    .catch(error => core.setFailed(error.message))
